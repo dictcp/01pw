ad09a8501cb6ee8466e35fa86a85c4112722483f
Dick Tang <dick.tang@ymail.com>
2018-05-18 22:19:00 +0800

    initial commit with (poor) web interface

--- README.md
+++ README.md
@@ -6,10 +6,10 @@ Leveraging SOPS, we can potentially to keep a vault file with encrpyted password
 
 To test this proof-of-concept, you may try
 ```
-# sops --encrypt --pgp F9E9048752EB069F example.json.plaintext > example.json
-# php poc.php 2>> /dev/null
-Vault for Site Reliability Engineer
-================
-- MYSQL_PASSWORD (MySQL root password): password
+$ sops --encrypt --pgp F9E9048752EB069F example.json.plaintext > example.json
+$ php -S 127.0.0.1:8080 poc.php 2>> /dev/null
 ```
+then open the web http://127.0.0.1:8080/
+
+![Image of PoC](https://rawgit.com/dictcp/01pw/master/poc.png)
 
--- poc.php
+++ poc.php
@@ -1,31 +1,87 @@
 <?php
 
-function decrypt($filename, $key, $email) {
+function decrypt($filename, $key, $email)
+{
     $plaintext = shell_exec("sops --decrypt $filename");
     $plainvault = json_decode($plaintext, true);
 
-    if (($plainvault["data"]["acl"][$email] ?? false) !== $email)
+    if (($plainvault["data"]["acl"][$email] ?? false) !== $email) {
         throw new \Exception("wrong email");
+    }
 
     return $plainvault["data"]["secret"][$key]['value'];
 }
 
-$vault_files = array_filter(scandir("./data"), function($x) {
-    return $x=='.'||$x=='..' ? false : true;
-});
+function get_vaults()
+{
+    $vaults = [];
+    $vault_files = array_filter(scandir("./data"), function ($x) {
+        return $x == '.' || $x == '..' ? false : true;
+    });
 
-foreach ($vault_files as $vault_file) {
-    $vault = json_decode(file_get_contents($vault_file), true);
+    foreach ($vault_files as $vault_file) {
+        $vaults[] = json_decode(file_get_contents($vault_file), true);
+    }
+    return $vaults;
+}
+
+function format_vaults($vault) {
+    $name = $vault["data"]["name_unencrypted"];
     $description = $vault["data"]["description_unencrypted"];
     $secrets = $vault["data"]["secret"];
 
-    echo "$description\n";
-    echo "================\n";
+    $buffer = "<div class=\"vault\">";
+    $buffer .= "<div class=\"name\">$name</div><div class=\"description\">$description</div>";
+
     foreach ($secrets as $key => $value) {
-        echo "- $key (";
-        echo $value["description_unencrypted"];
-        echo "): ";
-        echo decrypt($vault_file, $key, 'root@example.com');
-        echo "\n";
+        $buffer .= "<div class=\"item\">";
+        $buffer .= "<div class=\"key\">$key</div><div class=\"description\">{$value["description_unencrypted"]}</div>";
+        $buffer .= "<a href=\"#\" onClick=\"javascript:window.open('/get/secret?vault=$name&key=$key','secret','menubar=0,resizable=0,width=350,height=250')\">get password</a>";
+        $buffer .= "</div>";
     }
+    $buffer .= "</div>";
+    return $buffer;
+}
+
+function print_header() {
+    return <<<EOF
+<html>
+    <head>
+    <style>
+    .item { display: flex; justify-content: space-around; }
+    .key { display: flex; }
+
+    </style>
+    </head>
+    <body>
+EOF;
+}
+
+function print_footer() {
+    return <<<EOF
+    </body>
+</html>
+EOF;
+}
+
+// $email = $_SERVER['HTTP_X_FORWARDED_EMAIL']??'';
+$email = "root@example.com";
+
+if ($_SERVER['REQUEST_METHOD'] == 'GET' && !empty($email)) {
+    if ($_SERVER['REQUEST_URI'] == '/') {
+        $vaults = get_vaults();
+        echo print_header();
+        foreach ($vaults as $vault) {
+            echo format_vaults($vault);
+        }
+        echo print_footer();
+    } else if (preg_match('|/get/secret\?|', $_SERVER['REQUEST_URI'])) {
+        echo "<pre>";
+        echo decrypt("./data/".$_GET['vault'], $_GET['key'], $email);
+        echo "</pre>";
+    } else {
+        http_response_code(404);
+    }
+} else {
+    http_response_code(403);
 }
